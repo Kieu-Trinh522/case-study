24750101757d2decbd00219a3ef0b55ef288f59d
Kieu-Trinh522 <damkieutrinh522@gmail.com>
2021-01-04 12:26:46 +0700

    update

---
+++ resources/views/category/listCategory.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<form>
+    <a class="btn btn-success" href="{{route('category.create')}}">ADD</a>
+    <table class="table">
+        <tr>
+            <th>STT</th>
+            <th>NAME CATEGORY</th>
+            <th>ACTION</th>
+        </tr>
+        @forelse($categories as $key=>$category)
+            <tr>
+                <td>{{++$key}}</td>
+                <td>{{$category->category_name}}</td>
+                <td>
+                    <a class="btn btn-success" href="{{route('category.edit',$category->id)}}">EDIT</a>
+                    <a class="btn btn-danger" href="{{route('category.delete',$category->id)}}">DELETE</a>
+                </td>
+            </tr>
+        @empty
+        <tr>No data</tr>
+        @endforelse
+    </table>
+</form>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
