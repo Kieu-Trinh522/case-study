6f9ddbcd6b65e76e05c957b069f2665d6a2e8618
tongdung13 <tongvanthienvu@gmail.com>
2021-01-04 18:25:32 +0700

    update playlist

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -2,9 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Country;
+use App\Models\Playlist;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class PlaylistController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $playlists = Playlist::paginate(10);
+        $category = Category::all();
+        $country  = Country::all();
+
+        return view('playlists.list', compact('playlists', 'category', 'country'));
+    }
+
+    public function create()
+    {
+        $category = Category::all();
+        $country = Country::all();
+        return view('playlists.create', compact('category', 'country'));
+    }
+
+    public function store(Request $request)
+    {
+        $playlist = new Playlist();
+        $playlist->fill($request->all());
+
+        if($request->hasFile('audio')) {
+            $audio = $request->file('audio');
+
+            $filename = $audio->getClientOriginalName();
+
+            $location = public_path('audio/'. $filename);
+
+            $audio->move($location, $filename);
+
+            $playlist->audio = $filename;
+
+        }
+
+        if($request->hasFile('image')) {
+
+            $image = $request->file('image');
+            $path = $image->store('images', 'public');
+            $playlist->image = $path;
+        }
+
+
+        $playlist->save();
+
+        Session::flash('success', 'Upload with success');
+
+        return redirect()->route('playlists.index');
+
+
+    }
+
+    public function show($id){
+        $playlist = Playlist::findOrFail($id);
+
+        return view('playlists.playlist', compact('playlist'));
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Playlist extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'music_name',
+        'singer',
+        'category_id',
+        'country_id',
+        'image',
+        'audio'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function country()
+    {
+        return $this->belongsTo('App\Models\Country');
+    }
+
+
+
 }
