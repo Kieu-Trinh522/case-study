e259d8b85157c5bc20f51861d416e57b07fb6d49
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2021-01-09 13:33:32 +0700

    uploand1

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -142,7 +142,7 @@ class PlaylistController extends Controller
             return redirect()->route('playlists.index');
         }
         $playlist = Playlist::where('music_name', 'category_name','country_name','name_ambum','LIKE', '%'. $search . '%')->paginate(5);
-        
+
         $category = Category::all();
         $country = Country::all();
         $ambum = Ambum::all();
